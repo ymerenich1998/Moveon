fa7b39aadb1809ab68a6ec700f1abbf12f7f62a7
ymerenich1998 <ymerenich1998@gmail.com>
2019-12-16 23:02:02 +0200

    Add sidebar and navbar

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'activeadmin'
 gem 'devise'
+gem 'bootstrap', '~> 4.4.1'
+gem 'jquery-rails'
+gem 'font_awesome5_rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require font_awesome5
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
